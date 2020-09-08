826d0ef8d1a3597688237f1e3c40b88ca2180cb7
VFermat <vitor.eller30@gmail.com>
2020-09-07 21:10:21 -0300

    add patch request and models

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query, Body
 from pydantic import BaseModel, Field, ValidationError, validator, UUID1
-from typing import Optional
+from typing import Optional, Dict
 from enum import Enum
 import uvicorn
 import uuid
@@ -31,6 +31,24 @@ class DatabaseTaskModel(BaseModel):
     )
     status: TaskStatus = Field(TaskStatus.todo, description="Status of the task.")
 
+class UpdateTaskModel(BaseModel):
+    title: Optional[str] = Field(
+        ..., description="New title of the task.", min_length=3, max_length=120
+    )
+    description: Optional[str] = Field(
+        ..., description="New description of the task.", min_length=3, max_length=120
+    )
+    status: Optional[TaskStatus] = Field(TaskStatus.todo, description="New status of the task.")
+
+class UpdateTaskOut(BaseModel):
+    title: Optional[str] = Field(
+        ..., description="New title of the task.", min_length=3, max_length=120
+    )
+    description: Optional[str] = Field(
+        ..., description="New description of the task.", min_length=3, max_length=120
+    )
+    status: Optional[TaskStatus] = Field(TaskStatus.todo, description="New status of the task.")
+    message: Optional[str] = Field(..., description="This item appears in the situation of nonexistent task with the given UUID.")
 
 app = FastAPI(
     title="Nollo CRUD",
@@ -61,3 +79,32 @@ async def create_task(
     if dbTask.uuid not in db:
         db.update({dbTask.uuid: dbTask})
         return dbTask
+
+@app.patch("/task/{task_id}", response_model=DatabaseTaskModel)
+async def create_task(
+    task_id: str = Query(
+        ...,
+        description="Unique ID of the task you're patching",
+        example=""
+    ),
+    patch: UpdateTaskModel = Body(
+        ...,
+        description="Fields to be updated.",
+        example={
+            "title": "New Title",
+            "description": "New Description",
+            "status": "doing"
+        }
+    )
+):
+    global db
+
+    task = db.get(UUID1(task_id))
+    if task is not None:
+        task = task.dict()
+        task.update(**patch.dict())
+        dbTask = DatabaseTaskModel(**task)
+        db.update({UUID1(task_id): dbTask})
+        return dbTask
+        
+    return {"message": f"no task with id {task_id}"}
